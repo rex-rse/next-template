e49bceb95c63d7e495c5fd8658dfeb372ec0dac7
Carlos Graterol <carlos.graterol@icloud.com>
2022-08-01 17:42:53 -0400

    Fix card content placement

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -16,14 +16,14 @@ const Card = ({ title, data, icon, href }: TCardProps) => {
   return (
     <button
       onClick={handleClick}
-      className="flex aspect-[7/3] w-full rounded-lg  text-black shadow-lg hover:shadow-xl bg-gradient-to-l from-emerald-700 to-emerald-600 transition-all delay-100 duration-200 hover:from-emerald-600 hover:to-emerald-600"
+      className="flex aspect-[7/3] w-full rounded-lg  bg-gradient-to-l from-emerald-700 to-emerald-600 text-black shadow-lg transition-all delay-100 duration-200 hover:from-emerald-600 hover:to-emerald-600 hover:shadow-xl"
     >
-      <div className="flex w-1/3 items-center self-center justify-center p-2 text-white">
+      <div className="flex w-1/3 items-center justify-center self-center p-2 text-white">
         {icon}
       </div>
-      <div className="flex w-2/3 flex-col text-left h-full p-2">
-        <h1 className="lg:text-3xl text-2xl font-bold text-white">{title}</h1>
-        <h2 className="text-4xl lg:text-5xl text-white">{data}</h2>
+      <div className="flex h-full w-2/3 flex-col justify-center p-2 text-left">
+        <h1 className="text-2xl font-bold text-white lg:text-3xl">{title}</h1>
+        <h2 className="text-4xl text-white lg:text-4xl">{data}</h2>
       </div>
     </button>
   );
