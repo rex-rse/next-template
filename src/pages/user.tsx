2eec024c848cbe7874e571e358bc34a7e6ac4a01
Carlos Graterol <carlos.graterol@icloud.com>
2022-08-01 17:42:26 -0400

    Add error messages to user form

--- src/pages/user.tsx
+++ src/pages/user.tsx
@@ -1,9 +1,15 @@
-import LandingLayout from '@layouts/LandingLayout';
 import React, { ReactElement, useState } from 'react';
-import { useForm } from 'react-hook-form';
-import * as yup from 'yup';
-import { UserCircleIcon, PencilAltIcon } from '@heroicons/react/solid';
+import LandingLayout from '@layouts/LandingLayout';
 import InputV2 from '@components/inputs/InputV2';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useMutation } from 'react-query';
+import { requester } from 'utils/requester';
+import { useAppDispatch } from '@store/hooks';
+import { AxiosError } from 'axios';
+import { open } from '@store/counter/snackbarReducer';
+import { UserCircleIcon, PencilAltIcon } from '@heroicons/react/solid';
 import { Transition } from '@headlessui/react';
 
 interface Inputs {
@@ -11,33 +17,70 @@ interface Inputs {
   phone_number: string;
 }
 const Schema = yup.object().shape({
+  phone_number: yup
+    .number()
+    .test(
+      'len',
+      'Debe ser 11 dígitos',
+      (val) => val.toString().length === 11
+    )
+    .typeError('Debe ser un número')
+    .required('Este campo es requerido'),
   password: yup
     .string()
     .min(8, 'Mínimo 8 caracteres')
     .max(12, 'Máximo 12 caracteres')
     .required('Este campo es requerido'),
-  phone_number: yup
-    .number()
-    .min(11, 'Debe ser 11 digitos')
-    .max(11, 'Debe ser 11 digitos')
-    .required('Este campo es requerido'),
+  confirm_password: yup
+    .string()
+    .required('Este campo es requerido')
+    .oneOf([yup.ref('password'), 'Las contraseñas deben coincidir']),
 });
 
 const User = () => {
   const [isEditable, setIsEditable] = useState(false);
 
+  const dispatch = useAppDispatch();
+  const { mutate, isLoading } = useMutation(
+    (formData: Inputs) => {
+      return requester({
+        method: 'POST',
+        data: formData,
+        url: 'account-holder/update/',
+      });
+    },
+    {
+      onSuccess: (response) => {
+        const { data } = response;
+        console.log('response data', data);
+      },
+      onError: (error: AxiosError) => {
+        console.log(error.response.data);
+        dispatch(open({ text: error.response.statusText, type: 'error' }));
+      },
+    }
+  );
+
   const {
     register,
+    handleSubmit,
     formState: { errors },
-  } = useForm<any>({});
+  } = useForm<Inputs>({
+    resolver: yupResolver(Schema),
+  });
+
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    const { phone_number, password } = data;
+    mutate({ phone_number, password });
+  };
+
   return (
-    <div className="mt-36 w-5/6 rounded-xl bg-gray-100 p-24 shadow-xl">
+    <div className="mt-28 w-5/6 rounded-xl bg-gray-100 p-24 shadow-xl">
       <div className="flex items-center justify-between">
         <div className="border-r-2">
           <UserCircleIcon className="mr-16 h-20 pr-10 text-gray-500" />
         </div>
         <h1 className="ml-16 w-full text-center text-4xl font-bold tracking-wide">
-          {' '}
           John Doe
         </h1>
       </div>
@@ -48,18 +91,16 @@ const User = () => {
         </div>
         <div className="mt-10 flex items-start">
           {isEditable ? (
-            <form className="mr-auto">
-              <div className="flex w-full items-center">
-                <div className="w-full">
-                  <InputV2
-                    label="Teléfono celular"
-                    name="phone_number"
-                    type="text"
-                    errorMessage={errors.phone_number?.message}
-                    register={register}
-                  />
-                </div>
-                <div className="ml-16 w-full">
+            <form className="mr-auto" onSubmit={handleSubmit(onSubmit)}>
+              <div className="flex flex-col items-start">
+                <InputV2
+                  label="Teléfono celular"
+                  name="phone_number"
+                  type="text"
+                  errorMessage={errors.phone_number?.message}
+                  register={register}
+                />
+                <div className="mt-14 flex">
                   <InputV2
                     label="Contraseña"
                     name="password"
@@ -67,6 +108,19 @@ const User = () => {
                     errorMessage={errors.password?.message}
                     register={register}
                   />
+                  <div className="ml-16">
+                    <InputV2
+                      label="Confirmar Contraseña"
+                      name="confirm_password"
+                      type="text"
+                      errorMessage={
+                        // @ts-ignore
+                        errors.confirm_password &&
+                        'Las contraseñas deben coincidir'
+                      }
+                      register={register}
+                    />
+                  </div>
                 </div>
               </div>
               <input
@@ -91,19 +145,6 @@ const User = () => {
             <PencilAltIcon className="h-5 text-gray-600 hover:text-emerald-500" />
           </button>
         </div>
-        {/* <div className="mt-10 flex items-center">
-          <h3 className="mr-4 text-lg font-bold">Contraseña:</h3>
-          <h3 className="mr-auto text-lg">password</h3>
-          <button
-            type="button"
-            onClick={() =>
-              isPasswordEditable
-                ? setIsPasswordEditable(false)
-                : setIsPasswordEditable(true)
-            }
-          ></button>
-          <PencilAltIcon className="h-5 text-gray-600 hover:text-emerald-500" />
-        </div> */}
       </div>
     </div>
   );
